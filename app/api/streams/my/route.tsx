1df65ec55eddf5ea4b5b8e5da27afa5a03d57a3c
raviraj34 <singhraviraj19305@gmail.com>
2026-03-23 01:05:39 +0530

    dashboard_error

--- app/api/streams/my/route.tsx
+++ app/api/streams/my/route.tsx
@@ -2,30 +2,40 @@ import { prismaClient } from "@/app/lib/db";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest){
+export async function GET(req: NextRequest) {
     const session = await getServerSession();
 
     const user = await prismaClient.user.findFirst({
-        where:{
-            email:session?.user?.email ?? ""
+        where: {
+            email: session?.user?.email ?? ""
         }
     });
-    if(!user){
+    if (!user) {
         return NextResponse.json({
-            message:"Unauthenticated"
-        },{
-            status:403
+            message: "Unauthenticated"
+        }, {
+            status: 403
         })
     }
     const streams = await prismaClient.stream.findMany({
-        where:{
+        where: {
             userId: user.id
+        },
+        include: {
+            _count: {
+                select: {
+                    upvote: true
+                }
+            }
         }
     })
 
     return NextResponse.json({
-        streams
+        streams: streams.map(({ _count, ...rest }) => ({
+            ...rest,
+            upvotes: _count.upvote
+        }))
     })
 
 
-    }
+}
